52be3fc67ed2428ffe402038a9f592e2abc96db8
Sumedh <sumedh_kale@yahoo.com>
2022-05-18 18:11:15 -0700

    added readme file at index route

--- main.py
+++ main.py
@@ -1,10 +1,16 @@
 from app import create_app
+from markdown import markdown
 
 app = create_app()
 
+
 @app.route("/")
 def hello():
-    return "Hello! This is a basic Flask app. "
+    readme_file = open("README.md", "r")
+    md_template_string = markdown(
+        readme_file.read(), extensions=["fenced_code", "codehilite"]
+    )
+    return md_template_string
 
 
 if __name__ == "__main__":
